5aca11d4aa2e6a8d55cf983bd2e5308d6f7c8e36
David Pratt <david@midnightmechanism.com>
2023-07-23 21:42:21 -0500

    Add histogram for time series

--- scripts/predictionInterval.R
+++ scripts/predictionInterval.R
@@ -1,5 +1,9 @@
+# https://a-little-book-of-r-for-time-series.readthedocs.io/en/latest/src/timeseries.html
+
 kings <- scan("http://robjhyndman.com/tsdldata/misc/kings.dat",skip=3)
 print(kings)
+
+hist(kings, col = "Firebrick")
 
 
 
